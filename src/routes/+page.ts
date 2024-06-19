1678e31b23315e536afa867d8457d4317f43bbe1
Tom Wheeler <tom@temporal.io>
2024-06-18 21:20:47 -0500

    updated to add front page that makes you choose a role (in lieu of a login page)

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,5 +1,5 @@
 import { redirect } from '@sveltejs/kit';
 
 export function load() {
-	redirect(303, '/orders');
+	redirect(303, '/usertype');
 }
---
+++ src/routes/usertype/+page.svelte
@@ -0,0 +1,30 @@
+<svelte:head>
+	<title>OMS</title>
+	<meta name="description" content="OMS App" />
+</svelte:head>
+
+<section>
+	<h1 style="text-align: left; margin-bottom: 20px;">
+		Begin by selecting your role:
+	</h1>
+
+	<table style="width: 60%;">
+		<thead>
+			<tr>
+				<th>Role</th>
+				<th>Description</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td><a href="/orders">Customer</a></td>
+				<td>Someone who orders products from the store</td>
+			</tr>
+			<tr>
+				<td><a href="/shipments">Courier</a></td>
+				<td>Someone who delivers products to the customer</td>
+			</tr>
+		</tbody>
+	</table>
+
+</section>

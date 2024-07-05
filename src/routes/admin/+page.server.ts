ba4644810485c13af81f62ae7914638a5e216b08
Rob Holland <rob@temporal.io>
2024-07-05 16:35:37 +0100

    Add a maintenance mode toggle.
    Improve display of failed orders (for example payment failure).

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -1,7 +1,7 @@
 import { env } from '$env/dynamic/private';
 
 export const load = async () => {
-	const response = await fetch(`${env.FRAUD_API_URL}/limit`);
-	const { limit } = await response.json();
-	return { limit };
+	const response = await fetch(`${env.FRAUD_API_URL}/settings`);
+	const { limit, maintenanceMode } = await response.json();
+	return { limit, maintenanceMode };
 };
